82087a80f1b5b78770103b74256350655927bfba
Rodo Cabrera <rodolfocabrera007@gmail.com>
2023-12-07 21:36:33 -0300

    agragando controlador de subida de imagenes e icono, implementando cloudinary, multer y fs-extra

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,14 +1,19 @@
 const { Router } = require('express');
-const { getAllProducts, createProducts, getProductById, deleteProductById, editProductById } = require('../controllers/products.controller');
+const { getAllProducts, createProduct, getProductById, deleteProductById, editProductById, uploadIco, uploadImage } = require('../controllers/products.controller');
 const { getById } = require('../controllers/users.controllers');
 const { getProductByTittle, getProductByPrice, getProductByOfferprice, getProductByCategorie, getProductBySpotlight }= require('../controllers/products.controller');
+
+const multer = require('multer')
+const upload = multer({dest: "uploads/"})
 
 const route=Router();
 route.get('/', getAllProducts);
 route. get('/getById/:id', getProductById);
-route.post('/create', createProducts);
+route.post('/create', createProduct);
 route. delete('/delete/:id', deleteProductById);
 route.patch("/edit/:id", editProductById); 
+route.post("/upload-icon", upload.single("icon"), uploadIco);
+route.post("/upload-img", upload.array("images"), uploadImage);
 
 // creo rutas para mi shema model
 route.get('/gettittle/:tittle', getProductByTittle);
