61f57c7dc411f31fe9fe1626d22200ae41a735f3
Franco Chaile <francochaile2013.14@gmail.com>
2023-12-08 12:26:36 -0300

    actualizacion de back end

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,23 +1,84 @@
-const { Router } = require('express');
-const { getAllProducts, createProducts, getProductById, deleteProductById, editProductById } = require('../controllers/products.controller');
-const { getById } = require('../controllers/users.controllers');
-const { getProductByTittle, getProductByPrice, getProductByOfferprice, getProductByCategorie, getProductBySpotlight }= require('../controllers/products.controller');
+const { Router } = require("express");
+const {
+  getAllProducts,
+  createProducts,
+  getProductById,
+  deleteProductById,
+  editProductById,
+} = require("../controllers/products.controller");
+const { getById } = require("../controllers/users.controllers");
+const {
+  getProductByTittle,
+  getProductByPrice,
+  getProductByOfferprice,
+  getProductByCategorie,
+  getProductBySpotlight,
+} = require("../controllers/products.controller");
 
-const route=Router();
-route.get('/', getAllProducts);
-route. get('/getById/:id', getProductById);
-route.post('/create', createProducts);
-route. delete('/delete/:id', deleteProductById);
-route.patch("/edit/:id", editProductById); 
+const { validateRole, validatorToken } = require("../middlewares/auth");
+
+const route = Router();
+route.get("/"
+, validatorToken
+, validateRole
+, getAllProducts
+);
+
+route.get("/getById/:id"
+, validatorToken
+, validateRole
+, getProductById
+);
+
+route.post("/create"
+, validatorToken
+, validateRole
+, createProducts
+);
+
+route.delete("/delete/:id"
+, validatorToken
+, validateRole
+, deleteProductById
+);
+
+route.patch("/edit/:id"
+, validatorToken
+, validateRole
+, editProductById
+);
 
 // creo rutas para mi shema model
-route.get('/gettittle/:tittle', getProductByTittle);
-route.get('/getprice/:price', getProductByPrice);//offerPrice
-route.get('/getofferprice/:offerprice', getProductByOfferprice);
-route.get('/getcategorie/:categorie', getProductByCategorie);
-route.get('/getspotlight/:spotlight', getProductBySpotlight);
 
+route.get(
+  "/gettittle/:tittle",
+  validatorToken,
+  validateRole,
+  getProductByTittle
+);
 
+route.get("/getprice/:price"
+    , validatorToken
+    , validateRole
+    , getProductByPrice
+); //offerPrice
 
+route.get("/getofferprice/:offerprice"
+, validatorToken
+, validateRole
+, getProductByOfferprice
+);
 
-module.exports= route;
+route.get("/getcategorie/:categorie"
+, validatorToken
+, validateRole
+, getProductByCategorie
+);
+
+route.get("/getspotlight/:spotlight"
+, validatorToken
+, validateRole
+, getProductBySpotlight
+);
+
+module.exports = route;
